15318a4ec5d2b84b05b913da17b3783c79e6394f
dashlingv <daryasazina@gmail.com>
2026-04-13 22:47:07 +0500

    Параметризация pytest. Практика работы с pytest параметризацией

--- tests/test_autorization.py
+++ tests/test_autorization.py
@@ -2,7 +2,7 @@ import pytest
 from playwright.sync_api import Page, expect
 
 @pytest.mark.regression
-@pytest.mark.autorization
+@pytest.mark.authorization
 @pytest.mark.parametrize("email, password", [
     ("user.name@gmail.com", "password"),
     ("user.name@gmail.com", "  "),
